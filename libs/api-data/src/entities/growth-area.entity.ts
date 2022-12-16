c69c8bcb629791f8424fc4754dee3e0881244f6b
edevargas <sailedvc@gmail.com>
2022-12-16 18:07:25 -0500

    Add swagger descriptions for models

--- libs/api-data/src/entities/growth-area.entity.ts
+++ libs/api-data/src/entities/growth-area.entity.ts
@@ -1,4 +1,6 @@
+import { ApiProperty } from '@nestjs/swagger';
 export class GrowthArea {
+  @ApiProperty({ example: '9322c384-fd8e-4a13-80cd-1cbd1ef95ba8', description: "uuid"})
   id: string;
   name: string;
   description: string;
